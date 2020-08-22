3789f4ae136ea986c0e1220f78f23e78153771fb
David Castillo Alvarado <dcastilloa@uni.pe>
2020-08-22 18:07:01 -0500

    Prueba del gitupload se esta subiendo a los dos repositorios a la vez

---
+++ gitupload.sh
@@ -0,0 +1,12 @@
+git status
+git add .
+ECHO "Subiendo el codigo bruto"
+git commit
+git push origin master
+
+ECHO "Subiendo el compilado al static branch"
+cd public
+git checkout static
+git add .
+git commit
+git push origin static
